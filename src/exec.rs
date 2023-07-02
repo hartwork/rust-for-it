2c8bbb5213f2f2d92273c85a27394829143f6fd7
Sebastian Pipping <sebastian@pipping.org>
2023-07-03 00:26:26 +0200

    exec.rs: Extract submodule "tests"

--- src/exec.rs
+++ src/exec.rs
@@ -18,15 +18,6 @@ fn exit_code_from(exit_status: ExitStatus) -> i32 {
     }
 }
 
-#[cfg(test)]
-#[test]
-fn test_exit_code_from() {
-    assert_eq!(exit_code_from(ExitStatus::Exited(123)), 123);
-    assert_eq!(exit_code_from(ExitStatus::Signaled(2)), 130);
-    assert_eq!(exit_code_from(ExitStatus::Other(123)), 255);
-    assert_eq!(exit_code_from(ExitStatus::Undetermined), 255);
-}
-
 fn process_popen_result(popen_result: PopenResult<ExitStatus>, command: &str) -> i32 {
     match popen_result {
         Ok(exit_status) => exit_code_from(exit_status),
@@ -45,47 +36,64 @@ fn process_popen_result(popen_result: PopenResult<ExitStatus>, command: &str) ->
     }
 }
 
-#[cfg(test)]
-#[test]
-fn test_process_popen_result() {
-    let command = "command1";
-    assert_eq!(
-        process_popen_result(Ok(ExitStatus::Exited(123)), command),
-        123
-    );
-
-    let permission_denied = std::io::Error::new(ErrorKind::PermissionDenied, "error1");
-    assert_eq!(
-        process_popen_result(Err(PopenError::IoError(permission_denied)), command,),
-        126
-    );
-
-    let not_found = std::io::Error::new(ErrorKind::NotFound, "error2");
-    assert_eq!(
-        process_popen_result(Err(PopenError::IoError(not_found)), command,),
-        127
-    );
-
-    let other_error = std::io::Error::new(ErrorKind::BrokenPipe, "error3");
-    assert_eq!(
-        process_popen_result(Err(PopenError::IoError(other_error)), command),
-        255
-    );
-}
-
 pub(crate) fn run_command(command: &str, args: Vec<&str>) -> i32 {
     let popen_result = Exec::cmd(command).args(args.as_slice()).join();
     process_popen_result(popen_result, command)
 }
 
 #[cfg(test)]
-#[test]
-fn test_run_command_for_good() {
-    assert_eq!(run_command("sh", vec!["-c", "exit 0"]), 0);
-}
+mod tests {
+    use subprocess::ExitStatus;
+    use subprocess::PopenError;
 
-#[cfg(test)]
-#[test]
-fn test_run_command_for_bad() {
-    assert_eq!(run_command("sh", vec!["-c", "exit 123"]), 123);
+    use std::io::ErrorKind;
+
+    use super::exit_code_from;
+    use super::process_popen_result;
+    use super::run_command;
+
+    #[test]
+    fn test_exit_code_from() {
+        assert_eq!(exit_code_from(ExitStatus::Exited(123)), 123);
+        assert_eq!(exit_code_from(ExitStatus::Signaled(2)), 130);
+        assert_eq!(exit_code_from(ExitStatus::Other(123)), 255);
+        assert_eq!(exit_code_from(ExitStatus::Undetermined), 255);
+    }
+
+    #[test]
+    fn test_process_popen_result() {
+        let command = "command1";
+        assert_eq!(
+            process_popen_result(Ok(ExitStatus::Exited(123)), command),
+            123
+        );
+
+        let permission_denied = std::io::Error::new(ErrorKind::PermissionDenied, "error1");
+        assert_eq!(
+            process_popen_result(Err(PopenError::IoError(permission_denied)), command,),
+            126
+        );
+
+        let not_found = std::io::Error::new(ErrorKind::NotFound, "error2");
+        assert_eq!(
+            process_popen_result(Err(PopenError::IoError(not_found)), command,),
+            127
+        );
+
+        let other_error = std::io::Error::new(ErrorKind::BrokenPipe, "error3");
+        assert_eq!(
+            process_popen_result(Err(PopenError::IoError(other_error)), command),
+            255
+        );
+    }
+
+    #[test]
+    fn test_run_command_for_good() {
+        assert_eq!(run_command("sh", vec!["-c", "exit 0"]), 0);
+    }
+
+    #[test]
+    fn test_run_command_for_bad() {
+        assert_eq!(run_command("sh", vec!["-c", "exit 123"]), 123);
+    }
 }
